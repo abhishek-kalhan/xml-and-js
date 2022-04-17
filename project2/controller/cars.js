056517bd9dd2e50aaf186b2e1b44dd003271aea3
Abhishek Kalhan <abhishekkalhan@Abhisheks-Air.ht.home>
2022-04-17 14:31:52 -0400

    minor change for lower case

--- project2/controller/cars.js
+++ project2/controller/cars.js
@@ -7,16 +7,16 @@ const getAll = (data = {}) =>
 			result = result.filter((x) => x.id === parseInt(data.id));
 		}
 		if (data.carMake) {
-			result = result.filter((x) => x.carMake.toLowerCase().includes(data.carMake));
+			result = result.filter((x) => x.carMake.toLowerCase().includes(data.carMake.toLowerCase()));
 		}
 		if (data.carModel) {
-			result = result.filter((x) => x.carModel.toLowerCase().includes(data.carModel));
+			result = result.filter((x) => x.carModel.toLowerCase().includes(data.carModel.toLowerCase()));
 		}
 		if (data.carModelYear) {
 			result = result.filter((x) => x.carModelYear === parseInt(data.carModelYear));
 		}
 		if (data.color) {
-			result = result.filter((x) => x.color.toLowerCase().includes(data.color));
+			result = result.filter((x) => x.color.toLowerCase().includes(data.color.toLowerCase()));
 		}
 		resolve({ code: 200, data: result });
 	});
